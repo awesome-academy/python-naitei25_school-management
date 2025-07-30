94665c08a0fd6410b8e8b17909807e228b5eaf96
Thang.pt <thang.pt225927@sis.hust.edu.vn>
2025-07-30 14:59:57 +0700

    test

--- schoolmanagement/settings.py
+++ schoolmanagement/settings.py
@@ -17,7 +17,7 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 load_dotenv(dotenv_path=BASE_DIR / ".env")
-
+print("USING ENVIRONMENT VARIABLES", os.getenv("DB_USER"))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
@@ -86,6 +86,9 @@ DATABASES = {
         "PASSWORD": os.getenv("PASSWORD"),
         "HOST": os.getenv("HOST"),
         "PORT": os.getenv("PORT"),
+        'OPTIONS': {
+        'sslmode': 'require',
+        }
     }
 }
 
